7329cceaae1ad1b8118d0db349aa8f299607a00d
Pushkar Sharma <pushkarsharma1994@gmail.com>
2020-08-26 15:25:44 -0700

    Fixing the script

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name = 'numSave',
     version = '1.0.0',
-    packages=['wheel', 'click', 'flask', 'pymysql', 'sqlalchemy', 'uwsgi'],
+    install_requires=['wheel', 'click', 'flask', 'pymysql', 'sqlalchemy', 'uwsgi'],
 )
